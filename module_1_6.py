95f9369a6fc89d2588f157fbb60f5f675bea99a0
Nobel254 <alexey25475@gmail.com>
2024-11-12 22:27:56 +0300

    Add files via upload
    4

---
+++ module_1_6.py
@@ -0,0 +1,6 @@
+homework_made=12
+hour_spend=1.5
+name_course="Python"
+time_for_one_task=hour_spend/homework_made
+print("Курс:", name_course, "всего задач:",homework_made, "затрачено часов:", hour_spend,
+      "среднее время выполнения:", time_for_one_task,"часа.")
