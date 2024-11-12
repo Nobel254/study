059bc6143ea264aa8d64ec2b8a7896ec0dbf09f1
Nobel254 <alexey25475@gmail.com>
2024-11-12 23:55:29 +0300

    Add files via upload
    6

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+my_string=input()
+print('первоначальный вариант "',my_string,'"')
+print("Количество символов=",len(my_string))
+print("Верхний регистр",my_string.upper()) # верхний регистр
+print("Нижний регистр",my_string.lower()) # Нижний регистр
+print("Без пробелов",my_string.replace(" ","")) # без пробелов
+print("Первый символ",my_string[0])# Первый символ
+print("Последний символ",my_string[-1])# Последний символ
